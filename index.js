9bef0c6c4a92fe35c2416cb7480468765d8076f5
adam souzis <adam@souzis.com>
2017-07-29 15:22:15 -0700

    remove first implementation

--- index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./lib/srclink')('rebase', 'lib/configure.js');
--- test/fixtures/component.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  configDefaults: {},
-  name: 'test',
-  // when installing into a environment, use the associated modules
-  entryPoints: {
-    webapp: __dirname + '/transpiled',
-    test:   __dirname + '/transpiled'
-  }
-};
